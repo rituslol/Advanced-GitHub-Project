69230236e6104611c15a4228726fd71a37694ee4
rituslol <mborisova970@gmail.com>
2024-10-23 14:00:47 +0300

    Integrated helper function as submodule

--- index.js
+++ index.js
@@ -2,3 +2,7 @@ function helloWorld() {
     console.log("Welcome to Advanced GitHub project!");
 }
 helloWorld();
+
+import { greeting } from "../helper-functions3/greeting";
+
+console.log(greeting("GitHub User"));
